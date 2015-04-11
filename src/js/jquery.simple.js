0a2904cc7697e8869651fb0b38daf7f0ec315cc4
cgomez <cgomez@newhomestar.com>
2015-04-11 12:33:45 -0500

    Added core files and index.html

---
+++ src/js/jquery.simple.js
@@ -0,0 +1,38 @@
+/*
+ * Simple Video Player for JQuery Javascript Library
+ * http://www.christiangomez.me/projects/3
+ *
+ * Copyright (c) 2014 - 2015
+ * Licensed under the MIT license.
+ * http://opensource.org/licenses/MIT
+ *
+ * Author: Christian Gomez
+ * Version: 1.0.0.1
+ * Date: 11th April 2015
+ */
+
+ $.fn.simpleVideo = function(options) {
+	
+	var el = this; 
+
+
+	/* Initialize Video Player */
+	el.start = function()
+	{
+
+	}
+	el.stop = function()
+	{
+
+	}
+	el.changeVolume = function(volume)
+	{
+
+	}
+	el.skip = function(seconds)
+	{
+
+	}
+	/* Event Listeners */
+
+ }
