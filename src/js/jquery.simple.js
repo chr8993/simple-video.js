8deb26b0cf38f21abe7cb2586967233b6560abb2
Christian Gomez <chr8993@gmail.com>
2015-04-15 17:37:32 -0500

    Added volume toggle

--- src/js/jquery.simple.js
+++ src/js/jquery.simple.js
@@ -18,42 +18,48 @@
 	var timeHide 	=   null; //for controls
 
 	//create nodes
-	var _sVideo 	   =	document.createElement('video');
-	var _sControls     = 	document.createElement('div');
-	var _ul 		   =    document.createElement('ul');
-	var _sPlay         =    document.createElement('li');
-	var _sCenterP 	   =    document.createElement('div');
-	var _playButton    =    document.createElement('i');
-	var _sProgress	   =    document.createElement('li');
-	var _sAlign        =    document.createElement('div');
-	var _sAlignDiv     =    document.createElement('div');
-	var _sTime		   =    document.createElement('span');
-	var _sBarAlign     =    document.createElement('div');
-	var _sBarProgress  =    document.createElement('div');
-	var _sBarBuffer	   = 	document.createElement('div');
-	var _sBarPercent   =    document.createElement('div');
-	var _sVolume	   =    document.createElement('li');
-	var _sCenterV	   =    document.createElement('div');
-	var _volumeButton  =    document.createElement('i');
-	var _expandButton  =    document.createElement('i');
+	var _sVideo 	     =	  document.createElement('video');
+	var _sControls       = 	  document.createElement('div');
+	var _ul 		     =    document.createElement('ul');
+	var _sPlay           =    document.createElement('li');
+	var _sCenterP 	     =    document.createElement('div');
+	var _playButton      =    document.createElement('i');
+	var _sProgress	     =    document.createElement('li');
+	var _sAlign          =    document.createElement('div');
+	var _sAlignDiv       =    document.createElement('div');
+	var _sTime		     =    document.createElement('span');
+	var _sBarAlign       =    document.createElement('div');
+	var _sBarProgress    =    document.createElement('div');
+	var _sBarBuffer	     = 	  document.createElement('div');
+	var _sBarPercent     =    document.createElement('div');
+	var _sVolume	     =    document.createElement('li');
+	var _sCenterV	     =    document.createElement('div');
+	var _sVolumeControl  = 	  document.createElement('div');
+	var _sVolumeCon      = 	  document.createElement('div');
+	var _sVolumeBar      = 	  document.createElement('div');
+	var _volumeButton    =    document.createElement('i');
+	var _expandButton    =    document.createElement('i');
 
 	//assign class names
-	current.className	     =    "svContainer";
-	_sVideo.className        =    "sVideo";
-	_sControls.className     =	  "svControls";
-	_sPlay.className		 =    "sPlay";
-	_sCenterP.className		 = 	  "sCenter";
-	_playButton.className	 =    "fa fa-play";
-	_sProgress.className 	 =    "sProgress";
-	_sAlign.className 		 =    "sAlign";
-	_sBarAlign.className	 =    "sBarAlign";
-	_sBarProgress.className  =    "sBarProgress";
-	_sBarBuffer.className	 = 	  "sBarBuffer";
-	_sBarPercent.className 	 =    "sBarPercent";
-	_sVolume.className	     = 	  "sVolume";
-	_sCenterV.className		 = 	  "sCenter";
-	_volumeButton.className	 =    "fa fa-volume-up";
-	_expandButton.className	 =    "fa fa-expand";
+	current.className	       =    "svContainer";
+	_sVideo.className          =    "sVideo";
+	_sControls.className       =	"svControls";
+	_sPlay.className		   =    "sPlay";
+	_sCenterP.className		   = 	"sCenter";
+	_playButton.className	   =    "fa fa-play";
+	_sProgress.className 	   =    "sProgress";
+	_sAlign.className 		   =    "sAlign";
+	_sBarAlign.className	   =    "sBarAlign";
+	_sBarProgress.className    =    "sBarProgress";
+	_sBarBuffer.className	   = 	"sBarBuffer";
+	_sBarPercent.className 	   =    "sBarPercent";
+	_sVolume.className	       = 	"sVolume";
+	_sCenterV.className		   = 	"sCenter";
+	_sVolumeControl.className  = 	"sVolumeControl";
+	_sVolumeCon.className      = 	"sVolumeCon";
+	_sVolumeBar.className	   =    "sVolumeBar";
+	_volumeButton.className	   =    "fa fa-volume-up";
+	_expandButton.className	   =    "fa fa-expand";
 
 	//append children
 	current.appendChild(_sVideo);
@@ -72,6 +78,9 @@
 	_sBarProgress.appendChild(_sBarBuffer);
 	_ul.appendChild(_sVolume);
 	_sVolume.appendChild(_sCenterV);
+	_sCenterV.appendChild(_sVolumeControl);
+	_sVolumeControl.appendChild(_sVolumeCon);
+	_sVolumeCon.appendChild(_sVolumeBar);
 	_sCenterV.appendChild(_volumeButton);
 	_sCenterV.appendChild(_expandButton);
 
@@ -112,12 +121,6 @@
 		}
 	}
 
-	//change volume
-	el.changeVolume = function(volume)
-	{
-
-	}
-
 	//show controls
 	el.showControls = function(e)
 	{
@@ -151,13 +154,18 @@
 	//toggles the volume bar from showing
 	el.toggleVolume = function()
 	{
-
+		$(_sVolumeControl).toggleClass('active');
 	}
 
 	//changes the volume
-	el.changeVolume = function()
+	el.changeVolume = function(e)
 	{
-
+		//get offset set to volume
+		var height = e.offsetY;
+		$(_sVolumeBar).css('height', height + '%');
+		var volume = Math.abs(100 - height);
+		volume = volume/100;
+		_sVideo.volume = volume;
 	}
 
 	//show loading
@@ -221,10 +229,10 @@
 	/* Event Listeners */
 	_sPlay.addEventListener('click', el.toggleState, false);
 	_volumeButton.addEventListener('click', el.toggleVolume, false);
+	_sVolumeControl.addEventListener('click', el.changeVolume, false);
 	_sVideo.ontimeupdate = el.updateTime;
 	_sVideo.onended = el.stop;
 	_sBarProgress.addEventListener('click', el.skip, false);
-
 	//show controls on hover
 	current.addEventListener('mouseover', el.showControls, false);
 	current.addEventListener('mouseout', el.hideControls, false);
